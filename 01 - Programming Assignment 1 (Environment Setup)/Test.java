21af856dee0544e7f04a8213e6c6cd34c0801130
TaylorByrdUCF <54604354+TaylorByrdUCF@users.noreply.github.com>
2019-08-27 19:22:41 -0400

    Add files via upload

---
+++ 01 - Programming Assignment 1 (Environment Setup)/Test.java
@@ -0,0 +1,11 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("My name is Taylor Byrd.");
+		System.out.println("I am studying Java this semester!");
+
+	}
+
+}
